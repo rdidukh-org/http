cd693af340fa9cc3ff64cea30c5def498989d7c0
Roman Didukh <rdidukh@google.com>
2020-01-15 17:12:49 +0000

    Add linter.

--- server_test.js
+++ server_test.js
@@ -1,25 +1,28 @@
 const assert = require('assert')
+const mocha = require('mocha')
 const http = require('http')
 const server = require('./server')
 const url = require('url')
 
-describe('server', function () {
-    it('starts', function (done) {
-        srv = server.create()
+// TODO: https://medium.com/caffeine-and-testing/async-testing-with-mocha-with-callbacks-and-promises-5d0002661b3f
 
-        srv.listen(0, 'localhost', () => {
-            const srvUrl = new url.URL('http://' + srv.address().address + ':' + srv.address().port + '/')
+mocha.describe('server', function () {
+  mocha.it('starts', function (done) {
+    const srv = server.create()
 
-            http.get(srvUrl, function (res) {
-                srv.close()
-                done()
-            }).on('error', (e) => {
-                srv.close()
-                done(e)
-            })            
-        }).on('error', function (err) {
-            srv.close()
-            done(err)
-        })
+    srv.listen(0, 'localhost', () => {
+      const srvUrl = new url.URL('http://' + srv.address().address + ':' + srv.address().port + '/')
+
+      http.get(srvUrl, function (res) {
+        srv.close()
+        done()
+      }).on('error', (e) => {
+        srv.close()
+        done(e)
+      })
+    }).on('error', function (err) {
+      srv.close()
+      done(err)
     })
+  })
 })
