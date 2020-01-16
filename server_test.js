288124f5d40887ebc39e478cf0044447ff6739c0
Roman Didukh <roma.didukh@gmail.com>
2020-01-16 11:48:19 +0000

    Set up Mocha and linter.

--- server_test.js
+++ server_test.js
@@ -1,11 +1,35 @@
 const assert = require('assert')
+const mocha = require('mocha')
 const http = require('http')
 const server = require('./server')
+const url = require('url')
 
-srv = server.create()
-srv.listen(() => {
-    console.log('Started listening on ', srv.address())
+mocha.describe('server', function () {
+  var srv
 
-    // TODO: run tests.
+  mocha.before(function(done) {
+    srv = server.create()
+    done()
+  })
+
+  mocha.it('works', function (done) {
+    srv.listen(0, 'localhost', () => {
+      const addr = srv.address()
+      const srvUrl = new url.URL('http://' + addr.address + ':' + addr.port + '/')
+
+      http.get(srvUrl, function (res) {
+        assert.strictEqual(res.statusCode, 200)
+        done()
+      }).on('error', (err) => {
+        done(err)
+      })
+    }).on('error', function (err) {
+      done(err)
+    })
+  })
+
+  mocha.after(function(done) {
     srv.close()
+    done()
+  })
 })
