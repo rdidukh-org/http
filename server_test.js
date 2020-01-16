678f2cff18943749deecfad767b92bff8152eb45
Roman Didukh <rdidukh@google.com>
2020-01-16 11:40:00 +0000

    Fix server test.

--- server_test.js
+++ server_test.js
@@ -4,25 +4,32 @@ const http = require('http')
 const server = require('./server')
 const url = require('url')
 
-// TODO: https://medium.com/caffeine-and-testing/async-testing-with-mocha-with-callbacks-and-promises-5d0002661b3f
-
 mocha.describe('server', function () {
-  mocha.it('starts', function (done) {
-    const srv = server.create()
+  var srv
 
+  mocha.before(function(done) {
+    srv = server.create()
+    done()
+  })
+
+  mocha.it('works', function (done) {
     srv.listen(0, 'localhost', () => {
-      const srvUrl = new url.URL('http://' + srv.address().address + ':' + srv.address().port + '/')
+      const addr = srv.address()
+      const srvUrl = new url.URL('http://' + addr.address + ':' + addr.port + '/')
 
       http.get(srvUrl, function (res) {
-        srv.close()
+        assert.strictEqual(res.statusCode, 200)
         done()
-      }).on('error', (e) => {
-        srv.close()
-        done(e)
+      }).on('error', (err) => {
+        done(err)
       })
     }).on('error', function (err) {
-      srv.close()
       done(err)
     })
+  })
+
+  mocha.after(function(done) {
+    srv.close()
+    done()
   })
 })
